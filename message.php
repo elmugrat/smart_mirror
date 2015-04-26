28acb6059b60b4b154d09ca3a7a728374f6a5f91
Brian Kintz <elmugrat@gmail.com>
2015-04-26 18:54:17 +0200

    - Added responsive UI for changing message text

--- message.php
+++ message.php
@@ -11,33 +11,89 @@ switch ($_GET['action']) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Smart Mirror: Message</title>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+	<title>Smart Mirror: Message</title>	
 	<style type='text/css'>
-	input {
-		position: absolute;
-		top: 40%;
-		left: 30%;
-		width: 40%;
-		padding: 3px 6px;
-		font-size: 18pt;
-		outline: 0;
-		border: 1px solid #CCC;
-		border-radius: 5px;
-		box-shadow: 1px 1px 4px #CCC;
+	* {
+		margin:0;
+		padding:0;
+			
+		box-sizing: border-box;
+		-webkit-box-sizing: border-box;
 	}
 	
-	input:hover, input:active {
-		box-shadow: 1px 1px 4px #999;
+	html {
+	    height:100%;
+	}
+	
+	body {
+		background: -moz-radial-gradient(center, ellipse cover,  rgba(30,47,84,0.9) 0%, rgba(30,47,84,1) 100%);
+		background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,rgba(30,47,84,0.9)), color-stop(100%,rgba(30,47,84,1)));
+		background: -webkit-radial-gradient(center, ellipse cover,  rgba(30,47,84,0.9) 0%,rgba(30,47,84,1) 100%);
+		background: -o-radial-gradient(center, ellipse cover,  rgba(30,47,84,0.9) 0%,rgba(30,47,84,1) 100%);
+		background: -ms-radial-gradient(center, ellipse cover,  rgba(30,47,84,0.9) 0%,rgba(30,47,84,1) 100%);
+		background: radial-gradient(ellipse at center,  rgba(30,47,84,0.9) 0%,rgba(30,47,84,1) 100%);
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e61e2f54', endColorstr='#1e2f54',GradientType=1 );
+	}
+	
+	form {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 40%;
+		transform: translate(-50%, -50%);
+		-webkit-transform: translate(-50%, -50%);
+		-ms-transform: translate(-50%, -50%);
+	}
+
+	input {
+		width: 100%;
+		padding: 8px 0;
+		text-indent: 10px;
+		font-size: 16px;
+		outline: 0;
+		background: #FFF;
+		border: 1px solid #CCC;
+		border-radius: 5px;
+		box-shadow: inset 0 1px 3px #CCC, 0px 0px 3px #CCC;
 	}
 	
 	*::-webkit-input-placeholder {
-		color: #777;
+		color: #888;
+	}
+	*:-moz-placeholder {
+	    color: #888;
+	    opacity: 1
+	}
+	*::-moz-placeholder {
+	    color: #888;
+	    opacity: 1
+	}
+	*:-ms-input-placeholder {
+	    color: #888;
+	}
+	
+	/* Smartphones (portrait and landscape) ----------- */
+	@media only screen
+	and (min-device-width : 320px) 
+	and (max-device-width : 480px) {
+		form { 
+			width: 90%; 
+			top: 20%;
+		}
+	}
+ 
+	/* iPads (portrait and landscape) ----------- */
+	@media only screen
+	and (min-device-width : 768px) 
+	and (max-device-width : 1024px) {
+		form { width: 70%; }
 	}
 	</style>
 </head>
 <body>
 	<form action='message.php?action=set' method='post'>
-		<input type='text' name='message' placeholder='Message'></input>
+		<input type='text' name='message' placeholder='Message' autofocus >
 	</form>
 </body>
 </html>
